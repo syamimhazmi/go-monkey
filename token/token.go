fd9bbdfce83f325485d0e11ffdbf367a9bc1e28c
syamimhazmi <syamimhazmi@gmail.com>
2024-08-21 21:41:46 +0700

    add tokenize for lexer

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifier + literals
+	IDENT = "IDENT" // add, foobar, x, y...
+	INT   = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN  = "("
+	RPARENT = ")"
+	LBRACE  = "{"
+	RBRACE  = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
